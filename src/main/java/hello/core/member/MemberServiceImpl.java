191f367fd146322059188fde6eb85c73ad47ea57
jong won Kang <krnomad@gmail.com>
2022-09-27 06:32:41 +0900

    Implement AppConfig - DI

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,11 @@ package hello.core.member;
 
 // 구현체가 하나만 있다면 관례상 Impl을 자주 사용한다
 public class MemberServiceImpl implements MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
